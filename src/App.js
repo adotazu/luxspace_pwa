233b6647d51b235d1d23ccf9c4447ee2c1c378fb
adotazu <strd940@gmail.com>
2022-04-12 09:46:26 +0700

    Dukungan offline dengan workbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Arrived from './components/Arrived.js';
+import Asside from './components/Asside.js';
+import Browse from './components/Browse.js';
+import Clients from './components/Clients.js';
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
+import Hero from './components/Hero.js';
+// import Modal from './components/Modal.js';
+import './tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header />
+    <Hero />
+    <Browse />
+    <Arrived />
+    <Clients />
+    <Asside />
+    <Footer />
+    {/* <Modal /> */}
+    </>
   );
 }
 
